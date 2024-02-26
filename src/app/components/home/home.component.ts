b997c643f2288155ae175cc1aff427a24f69cb4f
Kauan Dias <kauandias772@gmail.com>
2024-02-26 07:16:03 -0300

    Add Button Primary

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
+    BtnPrimaryComponent,
     NgOptimizedImage
   ],
   templateUrl: './home.component.html',
